3a56fbcb97b78cda176cbd14e5f1517d2b54a716
Ubsefor <letzdropthebas@gmail.com>
2021-11-04 14:34:26 +0300

    Initial Commit

---
+++ DistOS-Task/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DistOS-Task
+//
+//  Created by Alexander Makhov on 04.11.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
